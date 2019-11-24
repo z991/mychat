c1de1275159f2032ff0bfb8cdcd6a66894138a4b
zhuxuanyu <1049939190@qq.com>
2019-11-24 16:24:12 +0800

    用接口发送消息给聊天室

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    url(r'^test/$', views.test, name='test'),
+    url(r'^test_message/ok/$', views.test_send, name='test_send'),
+
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
 
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.http import JsonResponse
 # Create your views here.
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
-import json
+
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
+
 def room(request, room_name):
-    print('fasong=====', room_name)
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+def test_send(request):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        "chat_test_chat",
+        {
+            "type": "chat_message",
+            'message': "用接口测试一下发送消息啦啦啦",
+            'destination': "test_chat",
+            'time': datetime.now().strftime(format("%Y-%m-%d %H:%M:%S"))
+        }
+    )
+    pass
+    return JsonResponse({"info": "成功"})
+
+def test(request):
+    return render(request, 'chat/test.html', {})
